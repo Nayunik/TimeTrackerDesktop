885b38f77e6d1ad004d0fcb5c1e3c5cf8db5ac4d
Nayunik <koryukin.danil@list.ru>
2023-03-25 15:15:35 +0500

    Добавление проверок в форме регистрации

--- FormRegister.cs
+++ FormRegister.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TimeTrackerDesktop.AuthClasses;
@@ -41,20 +42,36 @@ namespace TimeTrackerDesktop
 
         private void buttonRegister_Click(object sender, EventArgs e)
         {
-            //Нужна проверка на то, что пользователя с указанным логином нет
-
-
+            //Проверка, что поля не пустые
             if(string.IsNullOrEmpty(textBox2.Text) || string.IsNullOrEmpty(textBox3.Text) || string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox4.Text) || string.IsNullOrEmpty(textBox5.Text) || string.IsNullOrEmpty(textBox6.Text) || string.IsNullOrEmpty(textBox7.Text) || string.IsNullOrEmpty(textBox8.Text))
             {
                 MessageBox.Show("Заполните обязательные поля!");
                 return;
             }
-            //Нужна проверка на то, что пароли одинаковые
+            //Проверка, что пароли одинаковые
             if (textBox2.Text != textBox3.Text)
             {
                 MessageBox.Show("Пароли отличаются!");
                 return;
             }
+            //Проверка, что почта верна
+            if (!Regex.IsMatch(textBox6.Text, ".+@.+\\.[a-z]+"))
+            {
+                MessageBox.Show("Электронная почта не верна!");
+                return;
+            }
+            //Првоерка, что пользователя с данным логином нет
+            var res = database.SelectFunctionUsing("auth.\"user\" u where \'" + textBox1.Text.Trim() + "\' = u.login");
+            res.Read();
+            if (res.HasRows)
+            {
+                MessageBox.Show("Пользователь с данным логином существует!");
+                res.Close();
+                return;
+            }
+            res.Close();
+
+            MessageBox.Show("GOOD");
         }
 
         private void buttonClearAll_Click(object sender, EventArgs e)
