7dd06697cc8e86d37a257151b455f8a66d91bcfc
Nayunik <koryukin.danil@list.ru>
2023-04-03 19:18:55 +0500

    Формирование отчета второго вида

--- FormAnalyze.cs
+++ FormAnalyze.cs
@@ -107,10 +107,59 @@ namespace TimeTrackerDesktop
                 case 0:
                     sheet.Name = "Пользователь - Приложение";
 
-                    /*for (int i = 0; i < users.Length; i++)
+                    int indexX0 = 2;
+                    sheet.Cells[1, 1] = "Пользователь";
+                    sheet.Cells[1, 2] = "Дата";
+                    sheet.Cells[1, 3] = "Приложение";
+                    sheet.Cells[1, 4] = "Время работы";
+                    for (int i = 0; i < cellUsers.Count; i++)
                     {
-                        sheet.Cells[1, i] = users[i].ToString();
-                    }*/
+                        string userlogin = cellUsers[i].Value.ToString();
+                        var resultFunc = database.SelectFunction($"select u.login, t.date, t.description, sum(t.duration) \r\nfrom main.timer t inner join auth.\"user\" u on t.user_id = u.user_id\r\nwhere u.login = '{userlogin}' \r\nand t.\"date\" >= to_date('{textBoxDateStart.Text}', 'yyyy-mm-dd')\r\nand t.\"date\" <= to_date('{textBoxDateEnd.Text}', 'yyyy-mm-dd')\r\n\r\ngroup by u.login, t.date, t.description;");
+                        if (resultFunc != null)
+                        {
+
+                            while (resultFunc.Read())
+                            {
+                                sheet.Cells[indexX0, 1] = resultFunc.GetValue(0).ToString();
+                                sheet.Cells[indexX0, 2] = resultFunc.GetValue(1).ToString().Split(' ')[0];
+                                sheet.Cells[indexX0, 3] = resultFunc.GetValue(2).ToString();
+                                sheet.Cells[indexX0, 4] = resultFunc.GetValue(3).ToString();
+                                indexX0++;
+                            }
+                            resultFunc.Close();
+                        }
+
+                    }
+                    sheet.Columns[1].ColumnWidth = 14;
+                    sheet.Columns[2].ColumnWidth = 10;
+                    sheet.Columns[3].ColumnWidth = 55;
+                    sheet.Columns[4].ColumnWidth = 14;
+
+                    sheet.Cells.Font.Name = "Times New Roman";
+                    sheet.Cells.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+                    sheet.Cells.HorizontalAlignment = Excel.XlVAlign.xlVAlignCenter;
+
+                    Excel.Chart excelchart0 = (Excel.Chart)app.Charts.Add(Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+                    excelchart0.Activate();
+
+                    //Изменяем тип диаграммы
+                    //app.ActiveChart.ChartType = Excel.XlChartType.xlLineMarkers;
+
+                    //Перемещаем диаграмму на лист 1
+                    app.ActiveChart.Location(Excel.XlChartLocation.xlLocationAsObject, "Пользователь - ПРиложение");
+                    //Получаем ссылку на лист 1
+                    var excelsheets0 = workBook.Worksheets;
+                    sheet = (Excel.Worksheet)excelsheets0.get_Item(1);
+                    //Перемещаем диаграмму в нужное место
+                    sheet.Shapes.Item(1).IncrementTop(-300);
+                    sheet.Shapes.Item(1).IncrementLeft(200);
+
+                    //Задаем размеры диаграммы
+                    sheet.Shapes.Item(1).Height = 500;
+                    sheet.Shapes.Item(1).Width = 1000;
+
                     break;
                 //Пользователь - День
                 case 1:
